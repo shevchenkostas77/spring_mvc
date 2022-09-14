44276e587e79c122a7d6b2a83ce2d4a55a3b449c
shevchenkostas77 <shevchenkostas77@icloud.com>
2022-09-14 16:45:09 +0300

    first commit

---
+++ src/main/java/com/shevchenkostas77/spring/mvc/MyController.java
@@ -0,0 +1,13 @@
+package com.shevchenkostas77.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView() {
+        return "first-view";
+    }
+}
